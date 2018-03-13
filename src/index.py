0c05f97fe52292cec7570bc181cef506a47f65f1
ltaoo <litaowork@aliyun.com>
2018-03-13 17:39:33 +0800

    增加 flask-script

--- src/index.py
+++ src/index.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask.ext.script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
     return '<h1>Hello Flask!</h1>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    # app.run(host='0.0.0.0', debug=True)
+    manager.run()
