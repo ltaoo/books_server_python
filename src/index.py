b76b4ad3334ca92ebff7ed77cf4274b622b03319
ltaoo <litaowork@aliyun.com>
2018-03-13 18:30:06 +0800

    实现连接 mysql

--- src/index.py
+++ src/index.py
@@ -1,8 +1,25 @@
 from flask import Flask
-from flask.ext.script import Manager
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 manager = Manager(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:yimi@172.17.0.3:3306/books'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+db.create_all()
 
 @app.route('/')
 def index():
